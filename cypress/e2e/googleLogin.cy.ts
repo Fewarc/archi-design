e349f62d16bd56515c949242ef3daa7a7755f36e
Piotr Berezka <berezkapiotr@gmail.com>
2024-01-10 22:41:02 +0100

    add google credentials to cypress config

--- cypress/e2e/googleLogin.cy.ts
+++ cypress/e2e/googleLogin.cy.ts
@@ -1,12 +1,12 @@
-describe('e2e google login', () => {
-  it('renders needed elements', () => {
-    cy.visit('/');
-    cy.get('[data-testid="cypress-google-signin-button"]').should('exist');
-  })
+describe("e2e google login", () => {
+  it("renders needed elements", () => {
+    cy.visit("/");
+    cy.get('[data-testid="cypress-google-signin-button"]').should("exist");
+  });
 
-  it('logs in with google', () => {
-    cy.visit('/');
+  it("logs in with google", () => {
+    cy.visit("/");
     cy.get('[data-testid="cypress-google-signin-button"]').click();
-    cy.get('button.button').click();
-  })
-})
+    cy.get("button.button").click();
+  });
+});
