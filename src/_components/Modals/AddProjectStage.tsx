0ad1727d6fe75acfa7649cd44cc4bfd67d02dbca
Piotr Berezka <berezkapiotr@gmail.com>
2024-05-08 22:17:00 +0200

    refactor validation in forms

--- src/_components/Modals/AddProjectStage.tsx
+++ src/_components/Modals/AddProjectStage.tsx
@@ -2,38 +2,46 @@ import { ModalProps } from "@/utils/types";
 import ActionModal from "../ActionModal";
 import Button from "../Button";
 import Input from "../Input";
-import { useState } from "react";
-import { useValidation } from "@/utils/hooks";
 import { addStageSchema } from "@/utils/validation";
 import { api } from "@/utils/api";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 interface AddProjectStageProps extends ModalProps {
   projectId: string;
 }
+
+type AddStageSchemaType = z.infer<typeof addStageSchema>;
 
 const AddProjectStage: React.FC<AddProjectStageProps> = ({
   open,
   onClose,
   projectId,
 }) => {
-  const [stageName, setStageName] = useState("");
-
   const utils = api.useUtils();
 
   const { mutate: craeteStage } = api.projectStage.create.useMutation({
     onSuccess: () => {
       utils.projectStage.invalidate();
       onClose();
-      setStageName("");
     },
   });
 
-  const { errors, validate } = useValidation({
-    schema: addStageSchema,
-    onSuccess: (stage) => {
-      craeteStage(stage);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<AddStageSchemaType>({
+    resolver: zodResolver(addStageSchema),
+    defaultValues: {
+      projectId,
     },
   });
+
+  const onSubmit: SubmitHandler<AddStageSchemaType> = (data) =>
+    craeteStage(data);
 
   return (
     <ActionModal
@@ -42,25 +50,26 @@ const AddProjectStage: React.FC<AddProjectStageProps> = ({
       title="Dodaj"
       subtitle="Nowy etap projektu"
     >
-      <div className="mt-4 flex flex-col gap-y-4">
-        <Input
-          value={stageName}
-          onChange={(e) => setStageName(e.target.value)}
-          variant="border_label"
-          placeholder="Etap I"
-          label={<div className="text-xs font-semibold">Nazwa etapu</div>}
-          error={errors?.name?._errors}
-        />
-      </div>
-      <div className="flex justify-end gap-x-4">
-        <Button
-          onClick={() => validate({ projectId, name: stageName })}
-          variant="defualt"
-          className="mt-9 w-full rounded-full border-0 bg-archi-purple px-5 py-2 text-center font-medium text-white shadow-double md:w-fit"
-        >
-          Dodaj
-        </Button>
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mt-4 flex flex-col gap-y-4">
+          <Input
+            variant="border_label"
+            placeholder="Etap I"
+            label={<div className="text-xs font-semibold">Nazwa etapu</div>}
+            error={errors?.name?.message}
+            {...register("name")}
+          />
+        </div>
+        <div className="flex justify-end gap-x-4">
+          <Button
+            type="submit"
+            variant="defualt"
+            className="mt-9 w-full rounded-full border-0 bg-archi-purple px-5 py-2 text-center font-medium text-white shadow-double md:w-fit"
+          >
+            Dodaj
+          </Button>
+        </div>
+      </form>
     </ActionModal>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,33 @@
 import { useState } from "react";
 import { LockKeyhole, UserRound } from "lucide-react";
 import { loginSchema } from "@/utils/validation";
-import { useValidation } from "@/utils/hooks";
 import Logo from "@/_components/Logo";
 import GoogleSignIn from "@/_components/GoogleSignIn";
 import Divider from "@/_components/Divider";
 import Input from "@/_components/Input";
 import Button from "@/_components/Button";
 import { LayoutPage } from "@/utils/types";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type LoginSchemaType = z.infer<typeof loginSchema>;
 
 const Home: LayoutPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const { errors, validate } = useValidation({
-    schema: loginSchema,
-    onSuccess: () => {},
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<LoginSchemaType>({
+    resolver: zodResolver(loginSchema),
   });
+
+  // TODO: handle login
+  const onSubmit: SubmitHandler<LoginSchemaType> = (data) => null;
 
   return (
     <div className="mx-auto flex h-screen w-full items-center justify-center sm:max-w-[688px] sm:border-x sm:border-black">
@@ -28,49 +39,48 @@ const Home: LayoutPage = () => {
           <h1 className="w-full">Witaj ponownie!</h1>
           <h2 className="text-2xl">Zaloguj się, aby rozpocząć</h2>
         </div>
-        <div className="mt-16 flex w-full flex-col gap-y-4 sm:mt-4">
-          <GoogleSignIn redirectUrl="/projects" />
-          <Divider className="my-2 text-xs font-semibold">lub</Divider>
-          <Input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            variant="border_label"
-            placeholder="e-mail"
-            label={
-              <div className="flex items-center">
-                <UserRound size={16} className="-mt-[2px]" />
-                <div className="ml-px text-xs font-semibold">Adres e-mail</div>
-              </div>
-            }
-            error={errors?.email?._errors}
-          />
-          <Input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            password
-            className="mt-4"
-            variant="border_label"
-            placeholder="hasło"
-            label={
-              <div className="flex items-center">
-                <LockKeyhole size={16} className="-mt-[2px] mr-px" />
-                <div className="ml-px text-xs font-semibold">Hasło</div>
-              </div>
-            }
-            error={errors?.password?._errors}
-          />
-          <div className="flex w-full justify-end text-sm">
-            <Button variant="link" onClick={() => null}>
-              Nie pamiętasz hasła?
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mt-16 flex w-full flex-col gap-y-4 sm:mt-4">
+            <GoogleSignIn redirectUrl="/projects" />
+            <Divider className="my-2 text-xs font-semibold">lub</Divider>
+            <Input
+              variant="border_label"
+              placeholder="e-mail"
+              label={
+                <div className="flex items-center">
+                  <UserRound size={16} className="-mt-[2px]" />
+                  <div className="ml-px text-xs font-semibold">
+                    Adres e-mail
+                  </div>
+                </div>
+              }
+              error={errors?.email?.message}
+              {...register("email")}
+            />
+            <Input
+              password
+              className="mt-4"
+              variant="border_label"
+              placeholder="hasło"
+              label={
+                <div className="flex items-center">
+                  <LockKeyhole size={16} className="-mt-[2px] mr-px" />
+                  <div className="ml-px text-xs font-semibold">Hasło</div>
+                </div>
+              }
+              error={errors?.password?.message}
+              {...register("password")}
+            />
+            <div className="flex w-full justify-end text-sm">
+              <Button variant="link" onClick={() => null}>
+                Nie pamiętasz hasła?
+              </Button>
+            </div>
+            <Button className="text-base font-bold" type="submit">
+              Zaloguj się
             </Button>
           </div>
-          <Button
-            className="text-base font-bold"
-            onClick={() => validate({ email, password })}
-          >
-            Zaloguj się
-          </Button>
-        </div>
+        </form>
       </div>
     </div>
   );
